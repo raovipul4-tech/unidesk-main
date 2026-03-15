ee84f95194cbd4172243958cb5543832c5c7b70a
raovipul4-tech <raovipul4@gmail.com>
2026-03-15 12:03:06 +0530

    Your Google tag wasn't detected on your website.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import "./globals.css";
 import { AppGenProvider } from "./components/appgen-provider";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -12,11 +13,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KLVPG47S');`}} />
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KLVPG47S');`,
+          }}
+        />
         
         {/* Primary Meta Tags */}
         <title>UniDesk - All-in-One CRM & WhatsApp Business Platform</title>
