76e4863416104af9c889b167d949c2fe54cae959
raovipul4-tech <raovipul4@gmail.com>
2026-03-15 12:56:23 +0530

    I'm getting this server error, please fix it:
    I'm getting a deployment

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -260,10 +260,6 @@ export default function LoginPage() {
                   </div>
               </div>
           </div>
-      </div>
-
-        </div>
-    </div>
         </div>
       </div>
     </>
